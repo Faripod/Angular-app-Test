e0ae2d9ab9a7abc72b07f576a43b9b953537a9a6
elfarinaawy@gmail.com <Faripod>
2020-04-09 23:57:39 +0200

    Binding update

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'TestServer';
+
+  constructor() {
+    setTimeout(() => {this.allowNewServer = true;
+    }, 2000);
+   }
 
   ngOnInit(): void {
   }
 
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event){
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
---
+++ src/app/successfully-alert/successfully-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-successfully-alert',
+  templateUrl: './successfully-alert.component.html',
+  styleUrls: ['./successfully-alert.component.css']
+})
+export class SuccessfullyAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
