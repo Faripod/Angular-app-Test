09dd6807db92eec5aff7c3f84ef2b2460b5b3a43
elfarinaawy@gmail.com <Faripod>
2020-04-13 12:40:34 +0200

    Directive update

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
 })
 
 export class ServerComponent {
@@ -10,7 +11,15 @@ export class ServerComponent {
     serverId: number = 10;
     serverStatus: string = 'offline';
 
-    getServerStatus(){
+    constructor() {
+      this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
+    getServerStatus() {
       return this.serverStatus;
+    }
+
+    getColor() {
+      return this.serverStatus === 'online' ? 'green' : 'red';
     }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'TestServer';
+  serverCreated = false;
+  servers = ['TestServer 1', 'TestServer 2', 'TestServer 3'];
 
   constructor() {
     setTimeout(() => {this.allowNewServer = true;
@@ -19,7 +21,9 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onCreateServer(){
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
